784dffaa8548e1adb57041a7167ffb697253d1a1
George Niotis <geoniotis@yahoo.com>
2023-01-16 13:42:27 +0200

    fix: remove code

--- src/view/pages/login/index.jsx
+++ src/view/pages/login/index.jsx
@@ -1,18 +1,11 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
+import React from 'react'
 
 const Login = () => {
   
   // TODO: Add design
   return (
    null
-  );
+  )
 }
 
-export default Login;
+export default Login
