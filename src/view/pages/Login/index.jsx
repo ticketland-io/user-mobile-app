488685d6ff781322295f36328706746694198d71
George Niotis <geoniotis@yahoo.com>
2023-01-17 13:41:57 +0200

    fix: fix login code style

--- src/view/pages/Login/index.jsx
+++ src/view/pages/Login/index.jsx
@@ -1,20 +1,16 @@
-import React, {useCallback, useEffect} from 'react'
+import React, {useCallback, useContext, useEffect} from 'react'
 import {SafeAreaView, Platform} from 'react-native'
 import AsyncButton from '../../components/AsyncButton'
-import {GoogleSignin} from '@react-native-google-signin/google-signin';
-import auth from '@react-native-firebase/auth';
+import {Context} from '../../core/Store'
 
 const Login = () => {
-  const signIn = (provider) => useCallback(async () => {
+  const [state, dispatch] = useContext(Context)
+
+  const logIn = (provider) => useCallback(async () => {
     try {
       switch (provider) {
         case 'google': {
-          await GoogleSignin.hasPlayServices({showPlayServicesUpdateDialog: true});
-
-          const {idToken} = await GoogleSignin.signIn();
-          const googleCredential = auth.GoogleAuthProvider.credential(idToken);
-          const authres = auth().signInWithCredential(googleCredential);
-
+          await state.firebase.signInWithGoogle()
           break;
         }
         case 'facebook': {
@@ -30,20 +26,9 @@ const Login = () => {
     }
   }, [])
 
-  useEffect(() => {
-    if (Platform.OS === 'ios') {
-      GoogleSignin.configure();
-    } else {
-      GoogleSignin.configure({
-        webClientId: '1041096392429-t92h90h7naekc8arct1oteuc183qqhv9.apps.googleusercontent.com',
-      });
-    }
-  }, [])
-
-
   return (
     <SafeAreaView >
-      <AsyncButton loading={false} onPress={signIn('google')}>
+      <AsyncButton loading={false} onPress={logIn('google')}>
         Login
       </AsyncButton>
     </SafeAreaView>
