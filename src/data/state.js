6ad93fd22687efb2f182449ebe8f207a373b755e
George Niotis <geoniotis@yahoo.com>
2023-01-18 17:08:59 +0200

    fix: code style

--- src/data/state.js
+++ src/data/state.js
@@ -1,7 +1,6 @@
 import EutopicCore from '@ticketland-io/eutopic-core'
 import EutopicSolanaWallet from '@ticketland-io/eutopic-solana-wallet'
 import Enclave from '@ticketland-io/eutopic-web-enclave'
-// import FirebaseAuth from '@ticketland-io/eutopic-firebase-auth'
 import FirebaseAuth from '../data/record';
 
 const Wallet = () => EutopicSolanaWallet({enclave: Enclave()})
