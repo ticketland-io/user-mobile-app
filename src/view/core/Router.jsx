b8209e9e1e831a88089e63e1da866d5cf4105213
George Niotis <geoniotis@yahoo.com>
2023-01-19 14:42:37 +0200

    feat: adding profile route

--- src/view/core/Router.jsx
+++ src/view/core/Router.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import {View} from 'react-native'
 import {
-  BrowserRouter,
   Route,
   Routes,
   NativeRouter
 } from 'react-router-native'
 import Home from '../pages/Home'
 import Login from '../pages/Login'
+import Profile from '../pages/Profile'
 import useStyle from './styles'
 
 const Router = props => {
@@ -18,6 +18,7 @@ const Router = props => {
       <NativeRouter>
         <Routes>
           <Route exact strict path='/' element={<Login />} />
+          <Route exact strict path='/profile' element={<Profile />} />
           <Route exact strict path='/home' element={<Home />} />
         </Routes>
       </NativeRouter>
