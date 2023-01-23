d323b4aef70d20086454d4f9e73fd1d90db27729
George Niotis <geoniotis@yahoo.com>
2023-01-24 01:46:30 +0200

    fix: removing loading setter

--- src/view/components/Auth.jsx
+++ src/view/components/Auth.jsx
@@ -1,15 +1,17 @@
 import React, {useEffect, useContext} from 'react'
 import {Context} from '../core/Store'
-import {setUser, setLoading} from '../../data/actions'
+import {setUser} from '../../data/actions'
 
 const Auth = () => {
   const [state, dispatch] = useContext(Context)
 
   useEffect(() => {
     state.firebase.onUserChanged(currentUser => {
-      dispatch(setLoading(true))
-      dispatch(setUser(currentUser))
-      dispatch(setLoading(false))
+      if (currentUser) {
+        dispatch(setUser(currentUser))
+      } else {
+        dispatch(setUser({}))
+      }
     })
   }, [])
 
