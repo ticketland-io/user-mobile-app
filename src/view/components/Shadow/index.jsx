6dd8c973df5e982807a789c4348fc77fe570f9ee
George Niotis <geoniotis@yahoo.com>
2023-02-22 14:02:38 +0200

    fix: shadow comp

--- src/view/components/Shadow/index.jsx
+++ src/view/components/Shadow/index.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import {View} from 'react-native'
+import {Shadow as ShadowComp} from 'react-native-shadow-2'
+import {Context} from '../../core/Store'
 import useStyles from './styles'
 
 const Shadow = props => {
@@ -9,15 +11,30 @@ const Shadow = props => {
     children,
     ...rest
   } = props
+  const [state] = useContext(Context)
   const classes = useStyles()
 
-  return (
-    <View style={[classes.root, style]} {...rest}>
-      <View style={[classes.inner, styleInner]}>
-        {children}
+  const isDisabled = () => state.platform !== 'android' || style.shadowOpacity === 0
+
+  return isDisabled()
+    ? (
+      <View style={[classes.root, style]} {...rest}>
+        <View style={[classes.inner, styleInner]}>
+          {children}
+        </View>
       </View>
-    </View>
-  )
+    )
+    : (
+      <View {...rest}>
+        <ShadowComp style={classes.androidShadow} distance={2} disabled={isDisabled()} startColor={classes.androidShadow.shadowColor} offset={[5, 10]}>
+          <View style={[classes.root, style]}>
+            <View style={[classes.inner, styleInner]}>
+              {children}
+            </View>
+          </View>
+        </ShadowComp>
+      </View>
+    )
 }
 
 export default Shadow
--- src/view/components/Shadow/styles.js
+++ src/view/components/Shadow/styles.js
@@ -3,17 +3,19 @@ import {makeStyles} from '@rneui/themed'
 export default makeStyles(theme => ({
   root: {
     borderWidth: 1,
-    shadowColor: theme.palette.common.black,
+    shadowColor: theme.palette.common.shadow,
     shadowOffset: {width: 5, height: 10},
     shadowOpacity: 1,
     borderRadius: 16,
-    margin: 10,
-    marginBottom: 20,
     borderColor: theme.palette.common.yellow500,
     backgroundColor: theme.palette.common.white,
   },
   inner: {
     borderRadius: 16,
     overflow: 'hidden'
+  },
+  androidShadow: {
+    alignSelf: 'stretch',
+    shadowColor: theme.palette.common.shadow,
   }
 }))
